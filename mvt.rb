f5aa651769dc7be53c2e5987249ff2f8d586c5eb
Rahul Reddy <rahul.reddy@go-jek.com>
2019-01-31 12:18:35 +0530

    [Rahul] updated the sha to install 1.0.1.

--- mvt.rb
+++ mvt.rb
@@ -2,7 +2,7 @@ class Mvt < Formula
   desc "A command line tool for replacing 'Move to trash' option in mac."
   homepage "https://github.com/rahulreddyr3/MoveToTrash.git"
   url "https://github.com/rahulreddyr3/MoveToTrash/raw/master/archive/mvt-1.0.1.tar.gz"
-  sha256 "bbfd0f701382213d5d8261eeabc77e00c94cd0c570d128e582dd7d302c3171c8"
+  sha256 "2f49ce48550ee56f30ee2c323b5fe039e51ec001d63d720628498e8c5beaa313"
   version "1.0.1"
 
   bottle :unneeded
