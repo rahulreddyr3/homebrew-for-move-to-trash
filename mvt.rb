f63c895a028c082cd20c98e048d4e8d2d1d92790
Rahul <rahulreddyr3@gmail.com>
2019-05-27 00:35:56 +0530

    updated formula for mvt to install v1.0.5 directly from tag

--- mvt.rb
+++ mvt.rb
@@ -1,13 +1,14 @@
 class Mvt < Formula
   desc "A command line tool for replacing 'Move to trash' option in mac."
   homepage "https://github.com/rahulreddyr3/MoveToTrash.git"
-  url "https://github.com/rahulreddyr3/MoveToTrash/raw/master/archive/mvt-1.0.2.tar.gz"
-  sha256 "2cba41792b78db326e10444ba0e175f553a6e1bf0ced4e5c1c62ecfeb51a3a24"
-  version "1.0.2"
+  url "https://github.com/rahulreddyr3/MoveToTrash/archive/v1.0.5.tar.gz"
+  sha256 "c2d0d24d4ad75149b8f6c7615d4c5ac2214e6d0dbcb35abd4ac86392d60cc665"
+  version "1.0.5"
 
   bottle :unneeded
 
   def install
+    system "mv", "my_move_to_trash.sh", "mvt"
     bin.install "mvt"
   end
 end
