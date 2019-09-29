c16d37cf10cc6125f5e8dd963883ebe27d807395
Rahul <rahulreddyr3@gmail.com>
2019-09-29 21:59:09 +0530

    [Rahul] updated grit formula to install v1.1.2

--- grit.rb
+++ grit.rb
@@ -1,13 +1,14 @@
 class Grit < Formula
   desc "A command line tool to help keep your git green."
   homepage "https://github.com/rahulreddyr3/grit"
-  url "https://github.com/rahulreddyr3/grit/raw/master/archive/grit-1.0.0.tar.gz"
-  sha256 "09e2691e6eb892e712a81d9ff53775ddddc2345c6e3878a4bbbe9c63740b30d9"
-  version "1.0.0"
-
-  bottle :unneeded
+  url "https://github.com/rahulreddyr3/grit/archive/v1.1.2.tar.gz"
+  sha256 "261a24898f428fd86c23c4e81739d56cd7b425df178bf7c39f9f771623cdc627"
+  version "1.1.2"
 
   def install
-    bin.install "grit_init"
+    system "make"
+    bin.install "bin/grit"
+    prefix.install Dir["src"]
   end
 end
+
