27515f6e571843f8aeef974f14da1444ef3cfb81
Rahul <rahulreddyr3@gmail.com>
2019-12-01 13:20:15 +0530

    [Rahul] updated grit formula to install v1.1.4

--- grit.rb
+++ grit.rb
@@ -1,9 +1,9 @@
 class Grit < Formula
   desc "A command line tool to help keep your git green."
   homepage "https://github.com/rahulreddyr3/grit"
-  url "https://github.com/rahulreddyr3/grit/archive/v1.1.3.tar.gz"
-  sha256 "7e06137d3830df8c2f5654c5093ec3a80c09c278ca41441d69062530f9b749ac"
-  version "1.1.3"
+  url "https://github.com/rahulreddyr3/grit/archive/v1.1.4.tar.gz"
+  sha256 "7aa52cdce8a45b6801ad407010eed2970362436048da1cb432391e324a1a2d27"
+  version "1.1.4"
 
   def install
     system "make"
